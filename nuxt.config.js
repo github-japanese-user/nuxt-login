d602f512613bedf0b30b4b39e5b7638e3801c243
nakazawaken1 <nakazawaken1@gmail.com>
2020-10-25 07:32:19 +0000

    ci setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,5 +39,13 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  },
+
+  router: {
+    base: process.env.BASE
+  },
+
+  generate: {
+    dir: process.env.DIR
   }
 }
